41983fa916c2d36f6acbd8c3ff874e06b260773e
Mohammed <mohammedadell301@gmail.com>
2023-03-01 11:36:06 +0200

    books

--- models/books.js
+++ models/books.js
@@ -4,10 +4,11 @@ const bookScehma = new mongoose.Schema({
   title: String,
   description: String,
   categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
-  author: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "authors" },
   ratings: { type: mongoose.Schema.Types.ObjectId, ref: "ratings" },
   reviews: { type: mongoose.Schema.Types.ObjectId, ref: "reviews" },
   image: String,
+  status: { type: mongoose.Schema.Types.ObjectId, ref: "status" },
 });
 
 const bookModel = mongoose.model("book", bookScehma);
