efcfce1fbeebb530fa6981a532a9f813b7e6f185
Mohammed <mohammedadell301@gmail.com>
2023-03-03 16:33:01 +0200

    auth

--- models/users.js
+++ models/users.js
@@ -1,13 +1,13 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 const userschema = new mongoose.Schema({
-    firstname:{type:String,minlength:4,require:true},
-    lastname :{type:String,minlength:4,require:true},
-    email:{type:String,unique:true,match:/.+@.+\..+/},
-    password :{ type: String },
-    image:{ type: String },
-    token:{ type: String },
-})
+  firstname: { type: String, minlength: 4, require: true },
+  lastname: { type: String, minlength: 4, require: true },
+  email: { type: String, unique: true, match: /.+@.+\..+/ },
+  password: { type: String, require: true },
+  image: { type: String, require: true },
+  token: { type: String },
+});
 
-const usermodel = mongoose.model('users',userschema)
+const usermodel = mongoose.model("users", userschema);
 
-module.exports = usermodel
+module.exports = usermodel;
--- routes/register.js
+++ routes/register.js
@@ -12,28 +12,28 @@ router.post("/", async (req, res) => {
 
     if (!(email && password && firstname && lastname && image)) {
       res.status(400).send("All input is required");
+    } else {
+      const oldUser = await User.findOne({ email });
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+
+      encryptedPassword = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+        image,
+      });
+
+      const token = jwt.sign({ user_id: user._id, email }, TOKEN_KEY);
+
+      user.token = token;
+
+      res.status(201).json(user);
     }
-
-    const oldUser = await User.findOne({ email });
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
-
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      firstname,
-      lastname,
-      email: email.toLowerCase(),
-      password: encryptedPassword,
-      image,
-    });
-
-    const token = jwt.sign({ user_id: user._id, email }, TOKEN_KEY);
-
-    user.token = token;
-
-    res.status(201).json(user);
   } catch (err) {
     console.log(err);
   }
