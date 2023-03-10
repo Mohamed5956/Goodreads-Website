69f8bb6207259f86118fed41daffc80b6d713254
Mohammed <mohammedadell301@gmail.com>
2023-03-10 19:36:04 +0200

    image handling

---
+++ middlewares/upload.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const storage = (folderName) => multer.diskStorage({
+  destination: (req, file, cb) => {
+    const folderPath = `assets/uploads/${folderName}`;
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+    cb(null, folderPath);
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const filename = Date.now() + fileExt;
+    cb(null, filename);
+  },
+});
+
+const upload = (folderName) => multer({ storage: storage(folderName) });
+
+module.exports = upload;
--- routes/author.js
+++ routes/author.js
@@ -3,7 +3,9 @@ const admin = require("../middlewares/admin");
 const auth = require("../middlewares/auth");
 const router = express.Router();
 const authorModel = require("../models/author");
-
+const upload = require("../middlewares/upload");
+const fs = require("fs");
+const path = require("path");
 router.get("/", async (req, res) => {
   try {
     const authors = await authorModel.find({});
@@ -23,15 +25,25 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", admin, async (req, res) => {
-  const author = new authorModel(req.body);
-  try {
-    await author.save();
-    res.send(author);
-  } catch (e) {
-    res.send(e);
+router.post(
+  "/",
+  [admin, upload("author").single("photo")],
+  async (req, res) => {
+    const author = new authorModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      description: req.body.description,
+      birthDate: req.body.birthDate,
+      photo: req.file.filename,
+    });
+    try {
+      await author.save();
+      res.send(author);
+    } catch (e) {
+      res.send(e);
+    }
   }
-});
+);
 
 router.patch("/:id", admin, async (req, res) => {
   const id = req.params.id;
